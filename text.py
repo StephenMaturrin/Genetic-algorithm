ebcf3b65f4c051bb7c6fa1f2df6b142f5e3115a3
Maturrin <migue.solinas@gmail.com>
2018-01-04 19:34:55 +0100

    Starting 21

---
+++ text.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+# evenly sampled time at 200ms intervals
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+import future        # pip install future
+import builtins      # pip install future
+import past          # pip install future
+import six           # pip install six
+# Iterations;Meilleure Fitness;Moyenne Fitness
+
+
+file = open("graph1.txt", "r")
+
+aux_line = []
+y =[[] for _ in range(4)]
+# Moyenne Fitness
+
+
+for line in file:
+   aux_line = line.split(";")
+   print(aux_line)
+   y[0].append(float(aux_line[0]))
+   y[1].append(float(aux_line[1]))
+   y[2].append(float(aux_line[2]))
+   y[3].append(float(aux_line[3]))
+   # print(aux_line)
+   # print(aux_line[0]+"-"+aux_line[1]+"-"+aux_line[2])
+
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 30)
+
+# red dashes, blue squares and green triangles
+
+
+fig0 = plt.figure()
+ax0 = fig0.add_subplot(111)
+ax0.grid(True)
+gridlines = ax0.get_xgridlines() + ax0.get_ygridlines()
+plt.ylim(0,1.25)
+plt.yscale('linear')
+plt.xscale('linear')
+print(gridlines)
+for line in gridlines:
+    line.set_linestyle('-.')
+# ax0.grid(color='b', linestyle='-', linewidth=0.1)
+
+
+plt.plot(y[0], y[1], 'rs',y[0], y[1], 'r',y[0], y[2], 'g^',y[0], y[2], 'g',y[0], y[3], 'b',y[0], y[3], 'bX',)
+plt.ylabel('Fitness/Distance')
+plt.xlabel('Iterations')
+
+
+# Meilleure Fitness;Moyenne Fitness
+red_patch = mpatches.Patch(color='red', label='Meilleure Fitnes')
+green_patch = mpatches.Patch(color='green', label='Moyenne Fitness')
+blue_patch = mpatches.Patch(color='blue', label='Moyenne Distance')
+plt.legend(handles=[red_patch,green_patch,blue_patch])
+
+plt.show()
